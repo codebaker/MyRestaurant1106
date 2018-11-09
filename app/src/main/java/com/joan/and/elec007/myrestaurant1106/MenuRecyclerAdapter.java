2d0e4fc61a7f23a8a6c21ee91f6e4c23929f3cf7
codebaker <jungah.kim76@gmail.com>
2018-11-09 15:21:42 +0900

    center : LayoutManager추가 - DB연결 부분 Adapter로 넘김.

--- app/src/main/java/com/joan/and/elec007/myrestaurant1106/MenuRecyclerAdapter.java
+++ app/src/main/java/com/joan/and/elec007/myrestaurant1106/MenuRecyclerAdapter.java
@@ -1,5 +1,7 @@
 package com.joan.and.elec007.myrestaurant1106;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -14,13 +16,34 @@ import java.util.ArrayList;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
-public class MenuRecyclerAdapter extends RecyclerView.Adapter<MenuRecyclerAdapter.MenuViewHolder> implements View.OnClickListener{
+public class MenuRecyclerAdapter extends RecyclerView.Adapter<MenuRecyclerAdapter.MenuViewHolder> {
+    private DBOpenHelper db;
+    private SQLiteDatabase mdb;
+    private Cursor cursor;
+    private String sql;
 
-    ArrayList<Menu> menuList = null;
+    ArrayList<Menu> arrayList = null;
 
-    public MenuRecyclerAdapter(ArrayList<Menu> arrayList){
-        this.menuList = new ArrayList<>();
-        this.menuList = arrayList;
+    public MenuRecyclerAdapter(DBOpenHelper db){
+        this.db=db;
+        this.arrayList = new ArrayList<>();
+
+        mdb = db.getWritableDatabase();
+        sql = "SELECT * FROM MENU";
+        cursor = mdb.rawQuery(sql,null);
+
+        arrayList = new ArrayList<>();
+        Menu menu;
+        String seq,name;int cost;
+
+        while(cursor.moveToNext()){
+            seq = cursor.getString(cursor.getColumnIndex("menu_seq"));
+            name = cursor.getString(cursor.getColumnIndex("menu_name"));
+            cost = cursor.getInt(cursor.getColumnIndex("menu_cost"));
+
+            menu = new Menu(seq,name,cost);
+            arrayList.add(menu);
+        }
     }
 
     @NonNull
@@ -34,31 +57,31 @@ public class MenuRecyclerAdapter extends RecyclerView.Adapter<MenuRecyclerAdapte
 
     @Override
     public void onBindViewHolder(@NonNull MenuViewHolder holder, int position) {
-        Menu menu = menuList.get(position);
-        holder.textMenuSeq.setText(menu.getMenuName());
-        holder.textMenuCost.setText(menu.getMenuName());
+        Menu menu = arrayList.get(position);
+        holder.textMenuSeq.setText(menu.getMenuSeq());
+        holder.textMenuCost.setText(String.valueOf(menu.getMenuCost()));
         holder.textItemMenu.setText(menu.getMenuName());
         holder.textItemCount.setText("0");
-        holder.btnItemMinus.setOnClickListener(this);
-        holder.btnItemPlus.setOnClickListener(this);
+        final String name = menu.getMenuName();
+        holder.btnItemMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //minus button 클릭
+                //order seq 텍스트 에디터 확인하고 order seq가 없으면 새로 insert 한다.
+                Toast.makeText(v.getContext(),name+"minus",LENGTH_SHORT).show();
+            }
+        });
+        holder.btnItemPlus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),name+"plus",LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return menuList.size();
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btnItemMinus :
-                Toast.makeText(v.getContext(),"minus",LENGTH_SHORT).show();
-                break;
-            case R.id.btnItemPlus :
-                Toast.makeText(v.getContext(),"plus",LENGTH_SHORT).show();
-                break;
-        }
-
+        return arrayList.size();
     }
 
     public static class MenuViewHolder extends RecyclerView.ViewHolder{
--- app/src/main/java/com/joan/and/elec007/myrestaurant1106/OrderActivity.java
+++ app/src/main/java/com/joan/and/elec007/myrestaurant1106/OrderActivity.java
@@ -17,7 +17,7 @@ import android.widget.Spinner;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OrderActivity extends AppCompatActivity implements View.OnClickListener,AdapterView.OnItemSelectedListener{
+public class OrderActivity extends AppCompatActivity implements View.OnClickListener{
 
     private DBOpenHelper dbHelper;
     private SQLiteDatabase mdb;
@@ -44,46 +44,13 @@ public class OrderActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void setMenuRecyclerView() {
-        ArrayList<Menu> menuList = new ArrayList<>();
-        Menu menu = null;
-        menu = new Menu("3423","카페라떼",5000);
-        menuList.add(menu);
-        menu = new Menu("3424","아이스커피",3000);
-        menuList.add(menu);
-        menu = new Menu("3425","녹차라떼",4000);
-        menuList.add(menu);
+
 
         recyclerView = (RecyclerView) findViewById(R.id.rvMenuList);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        adapter = new MenuRecyclerAdapter(menuList);
+        adapter = new MenuRecyclerAdapter(dbHelper);
         recyclerView.setAdapter(adapter);
-    }
-
-    private void setMenu(){
-        mdb = dbHelper.getWritableDatabase();
-        sql = "SELECT * FROM MENU";
-        cursor = mdb.rawQuery(sql,null);
-        List<Menu> menuList = new ArrayList<>();
-
-        String[] MENUS = new String[5];
-        int i=MENUS.length-1;
-
-        String seq,name;int cost;
-        while(cursor.moveToNext()){
-            seq = cursor.getString(cursor.getColumnIndex("menu_seq"));
-            name = cursor.getString(cursor.getColumnIndex("menu_name"));
-            cost = cursor.getInt(cursor.getColumnIndex("menu_cost"));
-
-            Menu menu = new Menu(seq,name,cost);
-            menuList.add(menu);
-            MENUS[i] = menu.toString();
-            --i;
-        }
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.listview_order,MENUS);
-        ListView listView = findViewById(R.id.listViewMenu);
-        listView.setAdapter(arrayAdapter);
     }
 
     private void setTableseat() {
@@ -105,7 +72,7 @@ public class OrderActivity extends AppCompatActivity implements View.OnClickList
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,tableList);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinner.setAdapter(dataAdapter);
-        spinner.setOnItemSelectedListener(this);
+        //spinner.setOnItemSelectedListener(this);
     }
 
     @Override
@@ -114,13 +81,4 @@ public class OrderActivity extends AppCompatActivity implements View.OnClickList
         Intent intent = new Intent(this,MainActivity.class).putExtras(bundle);
         startActivityForResult(intent,RESULT_FIRST_USER);
     }
-
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {}
 }
